23f12640ac712ac9d28ab15f20ae9989efc23eaa
tommrot <tommasorotelli12@gmail.com>
2026-06-10 15:39:26 +0200

    fixed add and sub overflow check

--- int_overflow_check.c
+++ int_overflow_check.c
@@ -1,6 +1,14 @@
-int int_overflow_check(int a, int b){
+int intsub_overflow_check(int a, int b){
 	int diff =(int)((unsigned) a - (unsigned) b);
 	int neg_over = a < 0 && b > 0 && diff >= 0;
 	int pos_over = a > 0 && b < 0 && diff <= 0;
+	return !neg_over && !pos_over;
+}
+
+
+int intadd_overflow_checK(int a, int b){
+	int sum = (int)((unsigned)a + (unsigned)b);
+	int pos_over = a > 0 && b > 0 && sum < 0;
+	int neg_over = a < 0 && b < 0 && sum > 0;
 	return !neg_over && !pos_over;
 }
