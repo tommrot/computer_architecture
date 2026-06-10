25c3d56f5912e627e630d2494917fbe1626de873
tommrot <tommasorotelli12@gmail.com>
2026-06-10 14:37:53 +0200

    overflow_check fixed

--- int_overflow_check.c
+++ int_overflow_check.c
@@ -1,5 +1,6 @@
 int int_overflow_check(int a, int b){
-	int neg_over = a < 0 && b < 0 && (a + b) >= 0;
-	int pos_over = a > 0 && b > 0 && (a + b) < 0;
+	int diff =(int)((unsigned) a - (unsigned) b);
+	int neg_over = a < 0 && b > 0 && diff >= 0;
+	int pos_over = a > 0 && b < 0 && diff <= 0;
 	return !neg_over && !pos_over;
 }
