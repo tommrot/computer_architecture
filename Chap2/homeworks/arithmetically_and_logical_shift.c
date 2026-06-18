ceb3364b885889d5e12eccab438622cab0b1f8ae
tommrot <tommasorotelli12@gmail.com>
2026-06-18 17:56:29 +0200

    logical right shifting implemented

--- Chap2/homeworks/arithmetically_and_logical_shift.c
+++ Chap2/homeworks/arithmetically_and_logical_shift.c
@@ -11,14 +11,24 @@ int int_shifts_are_arithmetic(void){
 
 
 
-// write a function that applies arithmetically right shifting regardless by machine
-
+// write a function that applies arithmetically right shifting (insert 1 if msbit is 1, 0 if it's 0) to an int x regardless by machine
 
 int arithmetically_right_shift(int x, int k){
     if (int_shifts_are_arithmetic()) return x >> k;
     else {
-        if (((x >> (int)sizeof(int)) * 8 - 1) == 0) return x >> k;
+        if ((x >> ((int)sizeof(int) * 8 - 1)) == 0) return x >> k;
         else return (x >> k) | (~0 << (sizeof(int) * 8 - k));
+    }
+}
+
+
+// write a function that applies logical right shifting (insert 0 regardless by msbit value) to an int x regardless by machine
+
+int logical_right_shift(int x, int k){
+    if (!(int_shifts_are_arithmetic())) return x >> k;
+    else {
+        if (x >> ((int)sizeof(int) * 8 - 1) == 0) return x >> k;
+        else return (x >> k) & ~(~0 << ((int)sizeof(int) * 8 - 1));
     }
 }
 
